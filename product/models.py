1f5cf168a15f820f6c1715724ed70db0a0de93f1
bakayn29 <bakyn5@gmail.com>
2021-07-29 16:22:13 +0600

    Add Search, Pagination

--- product/models.py
+++ product/models.py
@@ -37,3 +37,6 @@ class Product(models.Model):
     def get_absolute_url(self):
         from django.urls import reverse
         return reverse('detail', kwargs={'id': self.pk})
+
+    class Meta:
+        ordering = ['-id']
